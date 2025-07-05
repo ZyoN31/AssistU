8462b545352b6d496e526f4ee4bfa010f8b15f5f
ZyoN31 <zyond31@gmail.com>
2025-07-05 12:59:26 -0600

    Exportando v1 AssistU hacia Github...

---
+++ src/main/java/com/assistuteam/assistu/AssistU.java
@@ -0,0 +1,10 @@
+package com.assistuteam.assistu;
+
+/** @author assistu_team **/
+
+public class AssistU {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
